26a24a1db92487990a87f7842f7b8976ffd7196e
Mayank <mayankwanjari45@gmail.com>
2025-03-03 18:00:05 +0530

    remove element

---
+++ Random-probs/Remove-element.c
@@ -0,0 +1,24 @@
+// rmoving element and updating the array
+1. int removeElement(int* nums, int numsSize, int val) {
+    int count=0;
+    for (int i=0;i<numsSize;i++){
+        if(nums[i]==val){
+            count++; 
+        }
+        else nums[i-count] = nums[i];
+    }
+    return (numsSize - count);
+}
+2. //OPTIMIZED
+int removeElement(int* nums, int numsSize, int val) {
+    int j = 0; // Pointer for placing non-val elements
+    
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] != val) {
+            nums[j++] = nums[i]; // Place non-val element at index j and move j forward
+        }
+    }
+    
+    return j; // j represents the new size of the modified array
+}
+
