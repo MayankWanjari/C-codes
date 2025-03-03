cd02fe6c3de80418bb870602bda934215e92a93e
Mayank <mayankwanjari45@gmail.com>
2025-03-03 12:05:20 +0530

    palindrome array

---
+++ Random-probs/palindrome-array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+    int palindrome(int* a,int n){
+            for (int i=0,j=n-1;i<j;i++,j--){
+                if(a[i]!=a[j]) {
+                    return 0;
+            }
+            
+        }
+    return 1;
+    }
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for (int i=0;i<n;i++) scanf("%d",&a[i]);
+    (palindrome(a,n))? printf("is palin"):printf("is not palin");
+
+    return 0;
+}
