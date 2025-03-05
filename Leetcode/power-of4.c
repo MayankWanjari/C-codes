1035df4d395c81803b9d5a5c8a594899d06d33a9
Mayank <mayankwanjari45@gmail.com>
2025-03-06 01:35:56 +0530

    power of 4

---
+++ Leetcode/power-of4.c
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/power-of-four?envType=problem-list-v2&envId=bit-manipulation
+
+/* bool isPowerOfFour(int n) {
+    if(n==0) return false;
+    if(n==1)
+        return true;
+    while(n%4==0){
+        n/=4;
+        if(n==1) return true;
+    }
+    return false;
+} 
+*/
