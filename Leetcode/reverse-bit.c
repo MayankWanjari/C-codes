2335d8aa6152de015d16a3fdac5bcd1ea8b7f4c8
Mayank <mayankwanjari45@gmail.com>
2025-03-06 01:34:40 +0530

    reverse the bit

---
+++ Leetcode/reverse-bit.c
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/reverse-bits?envType=problem-list-v2&envId=bit-manipulation
+
+uint32_t reverseBits(uint32_t n) {
+    uint32_t num=0,i=1;
+    while(i<=32){
+        num=(num<<1);
+        num=num | (n&1);
+        n=n>>1;
+        i++;
+    }
+    return num;
+}
