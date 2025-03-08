39921b6fe8e84fe02f8c2061230b3d1f41daad8b
Mayank <mayankwanjari45@gmail.com>
2025-03-08 15:25:08 +0530

    sorting the array

---
+++ Random-probs/sorting-array.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int a[n], i;
+
+    for (i = 0; i< n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    for (i = 0; i< n - 1; i++) {
+        int min = i;
+        for (int j=i+ 1; j < n; j++) {
+            if (a[j] < a[min]) {
+                min = j;
+            }
+        }
+        int temp = a[min];
+        a[min] = a[i];
+        a[i] = temp;
+    }
+
+    for (i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
