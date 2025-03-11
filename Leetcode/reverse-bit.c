35ecbac19d10b1e9c0f7aea163afe55b05b33cec
Mayank <mayankwanjari45@gmail.com>
2025-03-11 23:16:05 +0530

    Update reverse-bit.c

--- Leetcode/reverse-bit.c
+++ Leetcode/reverse-bit.c
@@ -1,5 +1,4 @@
 // https://leetcode.com/problems/reverse-bits?envType=problem-list-v2&envId=bit-manipulation
-// reverse array simplest code
 uint32_t reverseBits(uint32_t n) {
     uint32_t num=0,i=1;
     while(i<=32){
