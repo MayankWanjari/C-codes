0c7cf3045eff98c18756fc9511aaddb9b3b49163
Mayank <mayankwanjari45@gmail.com>
2025-03-13 02:11:44 +0530

    Create minimum-without-comparison.c

---
+++ HackerRank-Probs/minimum-without-comparison.c
@@ -0,0 +1,11 @@
+// Write a program to find the minimum of two integers a and b without using any comparison operators (<, >, <=, >=). Use only bitwise operators and logical operators to determine which number is smaller.
+
+// Input Format
+
+// Two space-separated integers a and b.
+// Constraints
+
+// −10^9 ≤ a,b ≤ 10^9
+// Output Format
+
+// Print the smaller of the two integers.
