5a16e63096f3c7f8bb892d728a894dd62ceb0500
Mayank <mayankwanjari45@gmail.com>
2025-03-12 17:32:35 +0530

    comments out

--- Leetcode/jump-game.c
+++ Leetcode/jump-game.c
@@ -1,12 +1,13 @@
 // https://leetcode.com/problems/jump-game?envType=problem-list-v2&envId=greedy
 
-// #include <stdio.h>
+#include <stdio.h>
 
-// bool canJump(int* nums, int numsSize) {
-//     int longest = 0;
-//     for (int i = 0; i < numsSize; i++) {
-//         if (i > longest) return false;
-//         longest = (longest > i + nums[i]) ? longest : (i + nums[i]);
-//     }
-//     return true;
-// }
+bool canJump(int* nums, int numsSize) {
+    int longest = 0;
+    for (int i = 0; i < numsSize; i++) {
+        if (i > longest) return false;
+        longest = (longest > i + nums[i]) ? longest : (i + nums[i]);
+    }
+    return true;
+}
+
