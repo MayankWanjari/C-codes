28a9dddc400eeed90785acee9e2ae217ca4f8a31
Mayank <mayankwanjari45@gmail.com>
2025-03-14 22:21:20 +0530

    Update check-if-two-numbers-are-differ-by-one-bit.c

--- HackerRank-Probs/check-if-two-numbers-are-differ-by-one-bit.c
+++ HackerRank-Probs/check-if-two-numbers-are-differ-by-one-bit.c
@@ -1,4 +1,14 @@
+// Write a program to check if two integers a and b differ by exactly one bit using bitwise operators. Two numbers differ by exactly one bit if their binary representations differ in only one position.
 
+// Input Format
+
+// Two space-separated integers a and b.
+// Constraints
+
+// −10^9 ≤ a,b ≤10^9
+// Output Format
+
+// Output "Yes" if the given numbers differ by exactly one bit, otherwise output "No".
 
 #include <stdio.h>
 
