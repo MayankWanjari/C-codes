41f7d94c1e9cab3b00a30cdc5314d321ad55f0a1
Mayank <mayankwanjari45@gmail.com>
2025-03-14 22:22:05 +0530

    Update check-for-single-set-bit.c

--- HackerRank-Probs/check-for-single-set-bit.c
+++ HackerRank-Probs/check-for-single-set-bit.c
@@ -1,4 +1,19 @@
+// An integer n is said to have exactly one set bit if its binary representation contains only a single 1. Write a program to check:
 
+// If n has exactly one set bit, return 1.
+// If n has more than one set bit or n is non-positive, return -1.
+// Input Format
+
+// A single integer n.
+
+// Constraints
+
+// −10^9 ≤ n ≤ 10^9
+
+// Output Format
+
+// Output 1 if n has exactly one set bit in its binary representation.
+// Output -1 if n has more than one set bit or is non-positive.
 
 #include <stdio.h>
 
