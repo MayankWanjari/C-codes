bd1ac003f2c8b5e550c29fd92440ff45dd44dfa3
Mayank <mayankwanjari45@gmail.com>
2025-03-14 22:20:45 +0530

    Update check-opposite-sign.c

--- HackerRank-Probs/check-opposite-sign.c
+++ HackerRank-Probs/check-opposite-sign.c
@@ -1,15 +1,4 @@
-// Write a program to determine if two integers a and b have opposite signs using bitwise operators.
 
-// Input Format
-
-// Two space-separated integers a and b.
-// Constraints
-
-// −10^9 ≤ a,b ≤ 10^9
-// Output Format
-
-// Print "Yes" if a and b have opposite signs.
-// Print "No" otherwise
 
 
 #include <stdio.h>
