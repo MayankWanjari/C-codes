32ea96903a5a856ddae705e3d29c15b41d7795b6
Mayank <mayankwanjari45@gmail.com>
2025-05-03 23:27:21 +0530

    commmit

---
+++ Codeforces TLE/target_practice.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+int main (){
+    int t;
+    cin >> t;
+    
+    while(t--){
+        int arr[10][10];
+
+        for(int i=0;i<10;i++){
+            for (int j=0;j<10;i++){
+                cin >> arr[i][j];
+                
+            }
+        }
+    }
+
+    return 0;
+}
