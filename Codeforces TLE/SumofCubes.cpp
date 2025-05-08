69c88029d60f0a1ee205a889b47475747b9aae80
Mayank <mayankwanjari45@gmail.com>
2025-05-08 15:12:01 +0530

    sum of cubes

--- Codeforces TLE/SumofCubes.cpp
+++ Codeforces TLE/SumofCubes.cpp
@@ -1,35 +1,68 @@
-#include <iostream>
-#include <cmath>
+// #include <iostream>
+// #include <cmath>
+// using namespace std;
+
+// int main() {
+//     int t;
+//     cin >> t;
+
+//     while (t--) {
+//         long long n;
+//         cin >> n;
+//         bool found = false;
+
+//         for (long long a = 1; a * a * a <= n; a++) {
+//             long long remaining = n - a * a * a;
+//             if (remaining <= 0) continue;
+
+//             long long b = round(cbrt(remaining));
+//             // Check nearby values to handle floating point inaccuracies
+//             for (long long delta = -2; delta <= 2; delta++) {
+//                 long long current_b = b + delta;
+//                 if (current_b <= 0) continue;
+//                 if (current_b * current_b * current_b == remaining) {
+//                     found = true;
+//                     break;
+//                 }
+//             }
+//             if (found) break;
+//         }
+
+//         cout << (found ? "YES\n" : "NO\n");
+//     }
+
+//     return 0;
+// }
+
+#include <bits/stdc++.h>
 using namespace std;
-
-int main() {
+ 
+int main()
+{
     int t;
-    cin >> t;
-
-    while (t--) {
-        long long n;
-        cin >> n;
-        bool found = false;
-
-        for (long long a = 1; a * a * a <= n; a++) {
-            long long remaining = n - a * a * a;
-            if (remaining <= 0) continue;
-
-            long long b = round(cbrt(remaining));
-            // Check nearby values to handle floating point inaccuracies
-            for (long long delta = -2; delta <= 2; delta++) {
-                long long current_b = b + delta;
-                if (current_b <= 0) continue;
-                if (current_b * current_b * current_b == remaining) {
-                    found = true;
-                    break;
-                }
+    cin>>t;
+    while(t--){
+        long long x;
+        cin>>x;
+        long long a=1;
+        long long b=1;
+        
+        while(b*b*b<=x) b++;
+        b--;
+        
+        int flag=0;
+        while(a<=b){
+            long long sum = pow(a, 3)+pow(b, 3);
+            if(sum==x){
+                flag=1;
+                break;
             }
-            if (found) break;
+            else if(sum<x) a++;
+            else b--;
         }
-
-        cout << (found ? "YES\n" : "NO\n");
+        if(flag) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
     }
-
+ 
     return 0;
 }
