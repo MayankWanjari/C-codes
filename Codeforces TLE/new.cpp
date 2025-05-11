a3ee91047e126120d04f02f6f13f7f4b69e9a40c
Mayank <mayankwanjari45@gmail.com>
2025-05-11 23:38:46 +0530

    new summ

--- Codeforces TLE/new.cpp
+++ Codeforces TLE/new.cpp
@@ -66,3 +66,39 @@ int main()
  
 
 }
+#include <iostream>
+using namespace std;
+
+
+int main (){
+    int t;
+    cin >> t;
+    
+    while(t--){
+        
+    }
+
+    return 0;
+}
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int t; // number of test cases
+    cin >> t;
+    while (t--) {
+        int a, b;
+        cin >> a >> b;
+
+        // Calculate absolute difference between a and b
+        int diff = abs(a - b);
+
+        // Each move can change a by up to 10 (either add or subtract)
+        // So minimum number of moves = ceil(diff / 10.0)
+        int moves = (diff + 9) / 10; // Equivalent to ceil(diff / 10)
+        
+        cout << moves << endl;
+    }
+    return 0;
+}
